58400f94f4a08be3df5017d6843e1e8a5a767863
Pablo Estigarribia <pablodav@gmail.com>
2014-10-27 09:21:30 -0200

    Added munin scripts
    Scripts to add new hosts.

---
+++ scripts/munin/addhoststomunin.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+# author: Pablo Estigarribia
+# license: GPLv3
+# This script adds hosts to munin, you can schedule it to just maintain a csv file with the values needed. 
+# it needs 2 template files to configure
+
+MUNINTEMPLATESNMPFILE=munin.template.snmp 
+MUNINTEMPLATEFILE=munin.template 
+MUNINCONFDIR=/etc/munin/munin-conf.d
+HOSTSFILE=/etc/hosts
+SNMPCOMMUNITY=public
+SNMPVERSION=2c
+file=$1
+
+function checksnmp() {
+   snmpstatus -c $SNMPCUMMUNITY -v $SNMPVERSION $NAME
+   if [[ $? -eq 0 ]] ; then 
+       SNMPRESULT="OK"
+   else 
+       #ping failed
+       echo "SNMP failed to $NAME"
+       SNMPRESULT="FAIL"
+       #exit "Error with SNMP"
+   fi
+}
+
+function checkping() {
+   ping -c 2 $NAME
+   if [[ $? -eq 0 ]] ; then 
+       PINGRESULT="OK"
+   else 
+       #ping failed
+       echo "Ping failed to $NAME"
+       PINGRESULT="FAIL"
+   fi
+}
+
+function addtohosts() {
+    if [[ "$PINGRESULT" == "OK" ]] ; then 
+        grep -q -i $NAME $HOSTSFILE
+        if [[ $? -eq 0 ]] ; then
+            # do nothing as it already is configured
+            echo "nameserver found in hosts file, do nothing"
+        else
+            # Start configuring new host
+            echo "nameserver not found, start adding to hosts"
+            echo "$IP    $NAME" >> $HOSTSFILE
+        fi
+    fi
+}
+
+function addmuninplugins() {
+   if [[ "$TYPE" == "SNMP" ]] ; then 
+       checksnmp
+       if [[ "$SNMPRESULT" == "OK" ]] ; then 
+           munin-node-configure --shell --snmp $NAME | xargs -L 1 xargs -t
+           cat $MUNINTEMPLATESNMPFILE | sed -e "s,TEMP_GROUP,$GROUP," -e "s,TEMP_NAME,$NAME," >> $MUNINCONFDIR/$GROUP_hosts.conf
+       fi
+   fi 
+}
+
+function addhostconfig() {
+   checkping
+   if [[ "$PINGRESULT" == "OK" ]] ; then 
+       cat $MUNINTEMPLATEFILE | sed -e "s,TEMP_GROUP,$GROUP," -e "s,TEMP_NAME,$NAME," >> $MUNINCONFDIR/$GROUP_hosts.conf
+   fi
+}
+
+# Reads file with csv values: GROUP;NAME;IP;TYPE 
+# if TYPE is SNMP it will configure plugins for SNMP
+cat $file | while read line;
+    do
+    echo "${line}"
+    GROUP=`echo ${line} | cut -d\; -f1`
+    NAME=`echo ${line} | cut -d\; -f2`
+    IP=`echo ${line} | cut -d\; -f3`
+    TYPE=`echo ${line} | cut -d\; -f4`
+
+    #Check if name is found in configuration
+    grep -q -i $NAME $MUNINCONFDIR/*
+    if [[ $? -eq 0 ]] ;
+        then
+        # do nothing as it already is configured
+        echo "nameserver found"
+    else
+        # Start configuring new host
+        echo "nameserver not found, start configuring"
+        addtohosts
+        addmuninplugins
+        addhostconfig
+    fi
+
+done
+
+
